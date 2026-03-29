3db36715c47b4166a0c79d1091c55e28a55387ff
JAmerico1898 <tesouraria.rj@gmail.com>
2026-03-29 14:54:22 -0300

    feat(mod1): add Remotion hero animation composition

---
+++ src/components/remotion/Mod1HeroAnimation.tsx
@@ -0,0 +1,56 @@
+import { AbsoluteFill, useCurrentFrame, interpolate } from "remotion";
+import { Mod1PricingCurves } from "./Mod1PricingCurves";
+import { Mod1HeroText } from "./Mod1HeroText";
+
+export const MOD1_COMP_WIDTH = 1920;
+export const MOD1_COMP_HEIGHT = 800;
+export const MOD1_FPS = 30;
+export const MOD1_DURATION_FRAMES = 450; // 15 seconds
+
+export function Mod1HeroAnimation() {
+  const frame = useCurrentFrame();
+
+  const gradientAngle = interpolate(frame, [0, 450], [135, 155], {
+    extrapolateRight: "clamp",
+  });
+
+  const bgShift = interpolate(frame, [0, 450], [0, 10], {
+    extrapolateRight: "clamp",
+  });
+
+  const maskOpacity = interpolate(frame, [160, 185], [0, 1], {
+    extrapolateLeft: "clamp",
+    extrapolateRight: "clamp",
+  });
+
+  return (
+    <AbsoluteFill
+      style={{
+        background: `linear-gradient(${gradientAngle}deg,
+          hsl(${220 + bgShift}, 60%, ${8 + bgShift * 0.3}%) 0%,
+          hsl(${230 + bgShift}, 30%, ${12 + bgShift * 0.2}%) 60%,
+          hsl(${210 + bgShift}, 50%, ${6 + bgShift * 0.1}%) 100%)`,
+        overflow: "hidden",
+      }}
+    >
+      <Mod1PricingCurves />
+
+      <div
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          width: "55%",
+          height: "100%",
+          background: `linear-gradient(to right,
+            hsl(${220 + bgShift}, 60%, ${8 + bgShift * 0.3}%) 50%,
+            transparent)`,
+          zIndex: 1,
+          opacity: maskOpacity,
+        }}
+      />
+
+      <Mod1HeroText />
+    </AbsoluteFill>
+  );
+}
